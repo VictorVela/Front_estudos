eb878eed210653bb3639fc28bfac4d1e6879c333
Victor Galhardo Vela <victorgvela@hotmail>
2019-02-05 09:09:47 -0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>exemplo de front</title>
+
+        <style type="text/css">
+            #box{
+                /*definimos a largura do box*/
+                width:300px;
+                /* definimos a altura do box */
+                height:100px;
+                /* definimos a cor de fundo do box */
+                background-color:#666;
+                /* definimos o quão arredondado irá ficar nosso box */
+                border-radius: 10px;
+            }
+        </style>
+
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+        <!-- iremos criar nossa div onde ficará o box arredondado -->
+        <div id="box">
+            <!-- basta colocar o svg aqui  -->
+            
+        </div>
+    </body>
+</html>
